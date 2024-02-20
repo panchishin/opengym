ce037d2606d6a07a1d75c770149d71559fcc2414
Anton Panchishin <anton.panchishin@uk.ey.com>
2024-02-20 11:59:43 -0500

    consistant beating 500 with total error calculation

--- torchtool.py
+++ torchtool.py
@@ -8,16 +8,17 @@ import copy
 
 class DQN():
     ''' Deep Q Neural Network class. '''
-    def __init__(self, state_dim, action_dim, hidden_dim=64, lr=0.05):
+    def __init__(self, state_dim, action_dim, hidden_dim, lr=0.01):
             self.loss = torch.nn.MSELoss()
             self.model = torch.nn.Sequential(
                             torch.nn.Linear(state_dim, hidden_dim),
                             torch.nn.LeakyReLU(),
                             # torch.nn.Tanh(),
                             torch.nn.Linear(hidden_dim, hidden_dim),
-                            torch.nn.LeakyReLU(),
-                            # torch.nn.Tanh(),
-                            torch.nn.Linear(hidden_dim, action_dim)
+                            # torch.nn.LeakyReLU(),
+                            torch.nn.Tanh(),
+                            torch.nn.Linear(hidden_dim, action_dim),
+                            torch.nn.Sigmoid(),
                     )
             self.optimizer = torch.optim.Adam(self.model.parameters(), lr)
 
@@ -87,9 +88,9 @@ class DQN_double(DQN):
         
     def replay(self, memory, size, gamma=1.0):
         ''' Add experience replay to the DQL network class.'''
-        if len(memory) >= size:
+        if True:  #len(memory) >= size:
             # Sample experiences from the agent's memory
-            data = random.sample(memory, size)
+            data = random.sample(memory, min(size, len(memory)))
             states = []
             targets = []
             # Extract datapoints from the data
@@ -102,7 +103,7 @@ class DQN_double(DQN):
                     # The only difference between the simple replay is in this line
                     # It ensures that next q values are predicted with the target network.
                     q_values_next = self.target_predict(next_state)
-                    q_values[action] = reward + gamma * torch.max(q_values_next).item()
+                    q_values[action] = (1-gamma)*reward + gamma * torch.max(q_values_next).item()
 
                 targets.append(q_values)
 
@@ -112,16 +113,14 @@ class DQN_double(DQN):
 
 def q_learning(env, model, episodes, gamma=0.9, 
                epsilon=0.3, eps_decay=0.99,
-               replay=False, replay_size=20, 
+               replay=False, replay_size=10,
                title = 'DQL', double=False, 
                n_update=10, verbose=True, memory=None):
     """Deep Q Learning algorithm using the DQN. """
     final = []
     memory = memory or []
-    episode_i=0
-    sum_total_replay_time=0
-    for episode in range(episodes):
-        episode_i+=1
+
+    for episode in range(1,episodes+1):
         if double:
             # Update target network every n_update steps
             if episode % n_update == 0:
@@ -131,8 +130,11 @@ def q_learning(env, model, episodes, gamma=0.9,
         state = env.reset()[0]
         done = False
         total = 0
+        model_updates = 0
+
+        total_error = 0
         
-        while not done and total < 1500:
+        while not done and total < 500:
             # Implement greedy search policy to explore the state space
             if random.random() < epsilon:
                 action = env.action_space.sample()
@@ -143,32 +145,31 @@ def q_learning(env, model, episodes, gamma=0.9,
             # Take action and add reward to total
             next_state, reward, done = env.step(action)[:3]
             if done:
-                reward = -5
+                reward = 0
             
             # Update total and memory
             total += reward
-            memory.append((state, action, next_state, reward, done))
             q_values = model.predict(state).tolist()
-             
-            if done:
-                if not replay:
-                    q_values[action] = reward
-                    # Update network weights
-                    model.update(state, q_values)
-                    break
+            q_values_next = model.predict(next_state)
+            predicted_reward = torch.max(q_values_next).item()
 
-            if replay:
-                t0=time.time()
-                # Update network weights using replay memory
-                if epsilon != 0:
+            total_error += math.pow(predicted_reward - reward,2)
+            # if math.pow(predicted_reward - reward,2) > 0.04:
+            if total_error > 0.04:
+                total_error = 0
+                if replay:
+                    memory.append((state, action, next_state, reward, done))
+                    memory = memory[-replay_size*2:]
                     model.replay(memory, replay_size, gamma)
-                t1=time.time()
-                sum_total_replay_time+=(t1-t0)
-            else: 
-                # Update network weights using the last step only
-                q_values_next = model.predict(next_state)
-                q_values[action] = reward + gamma * torch.max(q_values_next).item()
-                model.update(state, q_values)
+                else:
+                    q_values[action] = reward if done else (1-gamma)*reward + gamma*predicted_reward
+                    model.update(state, q_values)
+                model_updates += 1
+            else :
+                total_error *= 0.9
+
+            if done and not replay:
+                break
 
             state = next_state
         
@@ -177,11 +178,8 @@ def q_learning(env, model, episodes, gamma=0.9,
         final.append(total)
         
         if verbose:
-            print("episode: {}, total reward: {}".format(episode_i, total))
-            if replay:
-                print("Average replay time:", sum_total_replay_time/episode_i)
-        if total >= 1500:
-            break
+            print(f"episode: {episode}, epsilon {int(epsilon*1000)}/1000 model updates {model_updates} reward: {int(total)} " + "*"*int(total/5))
+
 
     plot_res(final, title)
 
@@ -198,31 +196,21 @@ if __name__ == '__main__':
     # Number of actions
     n_action = env.action_space.n
     # Number of episodes
-    episodes = 200
+    episodes = 500
     # Number of hidden nodes in the DQN
     n_hidden = 64
     # Learning rate
     lr = 0.001
     epsilon = 0.1
+    gamma = 0.9
 
-    """
+
     simple_dqn = DQN(n_state, n_action, n_hidden, lr)
-    simple = q_learning(env, simple_dqn, episodes, gamma=.9, epsilon=0.3)
+    simple = q_learning(env, simple_dqn, episodes, gamma=gamma, epsilon=epsilon, title='Simple DQL')
 
-
-    # Get replay results
     dqn_replay = DQN_replay(n_state, n_action, n_hidden, lr)
-    replay = q_learning(env, dqn_replay, 
-                        episodes, gamma=.8, 
-                        epsilon=epsilon, replay=True, 
-                        title='DQL with Replay')
+    replay = q_learning(env, dqn_replay, episodes, gamma=gamma, epsilon=epsilon, replay=True, title='DQL with Replay')
 
-
-    """
-    # Get replay results
     dqn_double = DQN_double(n_state, n_action, n_hidden, lr)
-    memory =  q_learning(env, dqn_double, episodes, gamma=.9, 
-                    epsilon=epsilon, replay=True, double=True,
-                    title='Double DQL with Replay', n_update=10)
-
+    memory =  q_learning(env, dqn_double, episodes, gamma=gamma, epsilon=epsilon, replay=True, double=True,title='Double DQL with Replay', n_update=10)
 
