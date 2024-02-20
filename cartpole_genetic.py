ccd8cd9bee9ce077d49a457a3c6fdf6cbbc0b0f9
Anton Panchishin <anton.panchishin@uk.ey.com>
2024-02-20 15:48:51 -0500

    make cartpole work again?

--- cartpole_genetic.py
+++ cartpole_genetic.py
@@ -1,14 +1,16 @@
-import pickle
 import gym
 from geneticalgorithm import GeneticAlgorithm, Phenotype
 
 phenotype_population = 50
 file_name = 'data/cartpole_genetic.pickle'
 
-env = gym.make('CartPole-v1')
-
 def oneTrial(phenotype, init_actions=[], demo=False) :
+    if demo :
+        env = gym.make('CartPole-v1', render_mode="human")
+    else:
+        env = gym.make('CartPole-v1')
     state = env.reset()
+
     total_reward = 0
 
     for count in range(501) :
@@ -20,12 +22,14 @@ def oneTrial(phenotype, init_actions=[], demo=False) :
         else :
             action = int(round( phenotype.compute(state)[0] ))
 
-        state, reward, done, info = env.step(action)
+        temp = env.step(action)
+        state, reward, done, info = temp[:4]
         total_reward += reward
 
         if done :
             return total_reward
 
+    env.close()
     return total_reward
 
 
@@ -34,12 +38,6 @@ def fitnessFunction(phenotype) :
 
 
 population = [ Phenotype() for i in range(phenotype_population) ]
-try :
-    with open(file_name, 'rb') as handle:
-        population = [pickle.load(handle)]
-except :
-    print("FAILED TO OPEN SAVE FILE")
-    pass
 
 ga = GeneticAlgorithm(
     mutationFunction = lambda phenotype: phenotype.mutate(),
@@ -59,23 +57,22 @@ print("Before evolving the best score is {:>3}, and mean score is {:>3}".format(
 print("")
 print("Starting the evolution process")
 print("")
-try :
-    for generation in range(0,501) :
+best_score_list = []
+mean_score_list = []
+for generation in range(0,501) :
         if generation % 5 == 0:
-            print("generation {:>3}, best score is {:>3}, and mean score is {:>3}".format(generation,int(ga.bestScore()),int(ga.meanScore())))
-            oneTrial(phenotype=ga.bestPhenotype(),demo=True)
-            if ga.bestScore() >= 499 :
+            best_score_list.append(ga.bestScore())
+            mean_score_list.append(ga.meanScore())
+            print("generation {:>3}, best score is {:>3}, and mean score is {:>3}".format(generation,int(best_score_list[-1]),int(mean_score_list[-1])))
+            # oneTrial(phenotype=ga.bestPhenotype(),demo=True)
+            if ga.bestScore() >= 299 :
                 print("Found a successful candidate")
                 for count in range(3,0,-1) :
                     print("Demoing it {} more times".format(count))
                     oneTrial(phenotype=ga.bestPhenotype(),demo=True)
                 break
         ga.evolve()
-except :
-    pass
 
-with open(file_name, 'wb') as handle:
-    pickle.dump(ga.bestPhenotype(),handle)
-
-env.close()
-
+from plottool import plot_res
+plot_res(best_score_list, "Best score")
+plot_res(mean_score_list, "Mean score")
