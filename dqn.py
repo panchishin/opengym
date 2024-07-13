f98e71f815f2e9ce594c5066e9526960d9955d80
Anton Panchishin <anton.panchishin@uk.ey.com>
2024-07-13 18:41:37 -0400

    remove ineffective regret impl

--- dqn.py
+++ dqn.py
@@ -53,26 +53,23 @@ class DQN():
 
 def q_learning(*, env, model:DQN, win_score:float=1000, episodes:int=500,
                gamma:float=0.95, epsilon:float=0.5, eps_decay:float=0.9, clip_size:float=0.2, error_threshold:float=0.03,
-               verbose:bool=False, optimistic_init:bool=True, use_regret:bool=False, optimism:float=0.9):
+               optimistic_init:bool=True, optimism:float=0.9):
     """Deep Q Learning algorithm using the DQN.
     gamma = the discount factor for future rewards
     epsilon = the exploration rate
     eps_decay = the rate at which epsilon decays
     clip_size = the maximum size of the update to the Q value
     error_threshold = the minimum error required to update the Q value
+    optimistic_init = whether to train the model to be optimistic about the rewards
+    optimism = the value to set the optimistic reward to
     """
 
     final = []
     win_count = 0
-    error_threshold = math.pow(error_threshold,2)
-
-    if use_regret:
-        regret_model = model.clone()
+    sqr_error_threshold = math.pow(error_threshold,2)
 
     if optimistic_init:
         model.optimistic_init(env, optimism=optimism)
-        if use_regret:
-            regret_model.optimistic_init(env, optimism=optimism)
 
     for episode in range(1,episodes+1):
         # Reset state
@@ -85,12 +82,7 @@ def q_learning(*, env, model:DQN, win_score:float=1000, episodes:int=500,
             # Epsilon-greedy
             explore = random.random() < epsilon
             if explore:
-                if use_regret:
-                    regret_predict = regret_model.predict(state)
-                    model_predict = model.predict(state)
-                    action = torch.argmax( np.square(regret_predict - model_predict) ).item()
-                else:
-                    action = env.action_space.sample()
+                action = env.action_space.sample()
             else:
                 action = torch.argmax(model.predict(state)).item()
             
@@ -105,7 +97,7 @@ def q_learning(*, env, model:DQN, win_score:float=1000, episodes:int=500,
             q_values_next = model.predict(next_state)
             predicted_reward = torch.max(q_values_next).item()
 
-            if math.pow(predicted_reward - reward,2) > error_threshold:
+            if math.pow(predicted_reward - reward,2) > sqr_error_threshold:
                 if done:
                     q_values[action] = reward
                 else:
@@ -115,27 +107,15 @@ def q_learning(*, env, model:DQN, win_score:float=1000, episodes:int=500,
                 model.update(state, q_values)
                 model_updates += 1
 
-            if explore and use_regret:
-                action_value = q_values[action]
-                q_values = regret_model.predict(state).tolist()
-                q_values_next = regret_model.predict(next_state)
-                q_values[action] = action_value
-                regret_model.update(state, q_values)
-
             state = next_state
         
         # Update epsilon
         epsilon = max(epsilon * eps_decay, 0.01)
         final.append(total)
         
-        if verbose:
-            print(f"episode: {episode}, epsilon {int(epsilon*100)}/100 model updates {model_updates} reward: {int(total)} " + "*"*int(total/20))
-
         if total >= win_score:
             win_count += 1
             if win_count >= 10:
-                if verbose:
-                    print(f"episode: {episode}, Task solved")
                 break
         else:
             win_count = 0
